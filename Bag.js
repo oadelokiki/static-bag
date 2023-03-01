fefc468871446e1edb985adbf3ae9f508a4a4555
ranktel <ranktel@worktop.myguest.virtualbox.org>
2023-03-01 13:24:18 -0500

    completed

--- Bag.js
+++ Bag.js
@@ -1,10 +1,17 @@
 class Bag {
-    constructor(weight) {
+   static maxWeight = 23;
+
+   constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
-        } 
+        }else if(weight > Bag.maxWeight){
+		throw new Error("Weight greater than max");
+	}
+	   
         this.weight = weight
     }
+
+
 }
 
 module.exports = Bag
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,16 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(20)
+        expect(bag.weight).toBe(20)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    })
+
+    test("bag smaller than max weight", () => {
+	expect(() => new Bag(25)).toThrowError("Weight greater than max");
+
     })
 })
